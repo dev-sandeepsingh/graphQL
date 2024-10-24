af96abff5aa816a06d300e95a0e137439281edf3
dev-sandeepsingh <sandeepsingh@founderandlightning.com>
2024-10-24 17:52:23 +0530

    Implemented backend GraphQl

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const {ApolloServer} = require('@apollo/server');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { default: axios } = require('axios');
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs:`
+        type Todo{
+        id: ID!
+        title: String!
+        completed: Boolean!
+        user: User
+        }
+
+        type User{
+        id: ID!
+        name: String!
+        email: String!
+        phone: String!
+        username: String!
+        }
+
+        type Query{
+            getTodos: [Todo]
+            getUsers: [User]
+            getUserById(id: ID!): User
+        }
+        `,
+        resolvers:{
+            Todo:{
+                user: async(todo)=> (await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.userId}`)).data
+
+            },
+            Query:{
+                getTodos: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data, 
+                getUsers: async () => (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
+                getUserById: async (parent, {id}) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data
+            }
+        }
+    });
+    
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    await server.start();
+
+    app.use("/graphql", expressMiddleware(server));
+
+    app.listen(8000, () => {
+        console.log("Server is running on port 8000");
+    });
+}
+
+startServer();
